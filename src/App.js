b8155e20c09da0b96811ef4a1fbb679e89fe196f
emre kılıç <emrekilic87@gmail.com>
2020-08-01 23:56:59 +0300

    url changed http to https

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-let url = "http://en.wikipedia.org/w/api.php";
+let url = "https://en.wikipedia.org/w/api.php";
 
 class App extends React.Component {
   constructor(props) {
